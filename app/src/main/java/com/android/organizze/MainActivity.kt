9d65272bad49b9cdf2973dfaaf10ad371aa4ecc8
Jardier <jardier@hotmail.com>
2020-07-21 16:54:48 -0300

    Adicionando o slider

--- app/src/main/java/com/android/organizze/MainActivity.kt
+++ app/src/main/java/com/android/organizze/MainActivity.kt
@@ -2,11 +2,38 @@
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.heinrichreimersoftware.materialintro.app.IntroActivity
+import com.heinrichreimersoftware.materialintro.slide.FragmentSlide
+import com.heinrichreimersoftware.materialintro.slide.SimpleSlide
 
-class MainActivity : AppCompatActivity() {
+  class MainActivity : IntroActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+
+        isButtonBackVisible = false;
+        isButtonNextVisible = false;
+
+        addSlide(FragmentSlide.Builder()
+            .background(android.R.color.white)
+            .fragment(R.layout.intro_1)
+            .build());
+
+        addSlide(FragmentSlide.Builder()
+            .background(android.R.color.white)
+            .fragment(R.layout.intro_2)
+            .build());
+
+        addSlide(FragmentSlide.Builder()
+            .background(android.R.color.white)
+            .fragment(R.layout.intro_3)
+            .build());
+
+        addSlide(FragmentSlide.Builder()
+            .background(android.R.color.white)
+            .fragment(R.layout.intro_4)
+            .build());
+
     }
 }
