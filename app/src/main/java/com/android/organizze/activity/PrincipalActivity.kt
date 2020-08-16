3feb66180958de99adad4c6e679e71371459823d
Jardier <jardier@hotmail.com>
2020-08-16 09:19:21 -0300

    Adicionando o FloatingActionMenu

--- app/src/main/java/com/android/organizze/activity/PrincipalActivity.kt
+++ app/src/main/java/com/android/organizze/activity/PrincipalActivity.kt
@@ -1,6 +1,8 @@
 package com.android.organizze.activity
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import com.android.organizze.R
@@ -14,10 +16,17 @@ class PrincipalActivity : AppCompatActivity() {
         setContentView(R.layout.activity_principal)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
+      /*  fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
-        }
+        }*/
     }
 
+    public fun adicionarDespesa(view: View) {
+        Toast.makeText(this,"Adicionar Despesa", Toast.LENGTH_LONG).show();
+    }
+
+    public fun adicionarReceita(view: View) {
+        Toast.makeText(this,"Adicionar Receita", Toast.LENGTH_LONG).show();
+    }
 }
